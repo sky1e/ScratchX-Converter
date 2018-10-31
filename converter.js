3310fca78de0e16f0625e746b60fa78ac7b0a5f0
Skye Jensen <skyeajensen@gmail.com>
2018-10-31 16:30:15 -0400

    Fix converter for scripts on sprites instead of stage. Also convert Finch obstacle blocks to booleans.

--- converter.js
+++ converter.js
@@ -1,4 +1,4 @@
-document.getElementById("file").onchange = function(ev) {
+document.getElementById("file").onchange = function (ev) {
     var files = ev.target.files;
 
     for (let i = 0; i < files.length; i++) {
@@ -9,22 +9,28 @@ document.getElementById("file").onchange = function(ev) {
 function handleFile(file) {
     JSZip
         .loadAsync(file)
-        .then(function(archive) {
+        .then(function (archive) {
 
             let project = archive.file("project.json");
             return project.async("text").then(JSON.parse)
                 .then(object => {
                     console.log(object);
-                    object.scripts.forEach(script => {
-                        script = script[2];
-                        script.forEach(fixBlock);
-                    });
+                    if (object.scripts) {
+                        fixScripts(object.scripts);
+                    }
+                    if (object.children) {
+                        object.children.forEach(child => {
+                            if (child.scripts) {
+                                fixScripts(child.scripts);
+                            }
+                        });
+                    }
                     object.info.savedExtensions.forEach(fixExtension);
                     return object;
                 }).then(JSON.stringify)
                 .then(file => {
                     archive.file("project.json", file);
-                    return archive.generateAsync({type: "blob"});
+                    return archive.generateAsync({ type: "blob" });
                 });
         }).then(blob => {
             saveAs(blob, file.name.replace(".sbx", ".converted.sbx"));
@@ -37,11 +43,18 @@ let blockReplacements = {
     "Hummingbird\u001fgetRaw": "Hummingbird\u001fgetRotary"
 };
 
+function fixScripts(scripts) {
+    scripts.forEach(script => {
+        script = script[2];
+        script.forEach(fixBlock);
+    });
+}
+
 function fixBlock(block) {
     if (block[0] in blockReplacements) {
         block[0] = blockReplacements[block[0]];
     }
-    for (let i = 1; i < block.length; i++) {
+    for (let i = 0; i < block.length; i++) {
         let slot = block[i];
         if (slot instanceof Array) {
             fixBlock(slot);
@@ -51,14 +64,20 @@ function fixBlock(block) {
 
 function fixExtension(extension) {
     if (extension.javascriptURL === "http://birdbraintechnologies.github.io/Chrome-Scratch-and-Snap-Support/Scratch Plugins/HummingbirdHID_Scratch(Chrome Plugin)/v0.7.2.js"
-    || extension.javascriptURL === "http://birdbraintechnologies.github.io/Chrome-Scratch-and-Snap-Support/Scratch Plugins/HummingbirdHID_Scratch(Chrome Plugin)/v0.7.2.js") {
+        || extension.javascriptURL === "http://birdbraintechnologies.github.io/Chrome-Scratch-and-Snap-Support/Scratch Plugins/HummingbirdHID_Scratch(Chrome Plugin)/v0.7.2.js") {
         extension.javascriptURL = "http://birdbraintechnologies.github.io/Chrome-Scratch-and-Snap-Support/Scratch Plugins/HummingbirdHID_Scratch(Chrome Plugin)/v1.0.js";
         extension.blockSpecs[8][2] = "getRotary";
         extension.blockSpecs[9][2] = "getLight";
     }
     if (extension.javascriptURL === "http://birdbraintechnologies.github.io/Chrome-Scratch-and-Snap-Support/Scratch Plugins/FinchHID_Scratch(Chrome Plugin).js"
-    || extension.javascriptURL === "http://birdbraintechnologies.github.io/Chrome-Scratch-and-Snap-Support/Scratch Plugins/FinchHID_Scratch(Chrome Plugin)/v0.7.5.js") {
+        || extension.javascriptURL === "http://birdbraintechnologies.github.io/Chrome-Scratch-and-Snap-Support/Scratch Plugins/FinchHID_Scratch(Chrome Plugin)/v0.7.5.js") {
         extension.javascriptURL = "http://birdbraintechnologies.github.io/Chrome-Scratch-and-Snap-Support/Scratch Plugins/FinchHID_Scratch(Chrome Plugin)/v1.0.js";
     }
+
+    if (extension.javascriptURL === "http://birdbraintechnologies.github.io/Chrome-Scratch-and-Snap-Support/Scratch Plugins/HummingbirdHID_Scratch(Chrome Plugin)/v1.0.js") {
+        extension.blockSpecs[4][0] = 'b';   // convert the obstacle blocks into booleans
+        extension.blockSpecs[5][0] = 'b';
+    }
+
     console.log(extension);
 }
